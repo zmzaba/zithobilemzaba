a0d0a12aad102523b3fce1e7a23130675577413f
Zithobile Mzaba <mzaba.zithobile2@gmail.com>
2025-05-25 12:47:42 +0200

    Add main class omitted in initial commit.

---
+++ src/main/java/com/zmzaba/banking/BankingAppApplication.java
@@ -0,0 +1,11 @@
+package com.zmzaba.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankingAppApplication.class, args);
+    }
+}
